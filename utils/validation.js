81d148794a5ff517e9349b95bf4480d9af92d931
ugurkarakurt <ugurkarakurt57@gmail.com>
2024-02-02 09:14:36 +0300

    Fix - #15 | Remove Empty func from validation.js

--- utils/validation.js
+++ utils/validation.js
@@ -18,12 +18,3 @@ export const validateForm = (formFields) => {
 
   return errors;
 };
-
-export const isEmpty = (obj) => {
-  for (const prop in obj) {
-    if (Object.hasOwn(obj, prop)) {
-      return false;
-    }
-  }
-  return true;
-}
